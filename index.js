9f55a8083281080aff2aceb91a66c25b6b130377
Vineet K Verma <vineetkverma1947@gmail.com>
2022-10-04 17:50:18 +0530

    some commit

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv').config()
 const express = require("express");
 const { route } = require('express/lib/router');
 const { default: mongoose } = require("mongoose");
-var compression = require("compression")
+const compression = require("compression")
 
 const mongoData = process.env.DATABASE_URL;
 const port = 3000;
-mongoose.connect(mongoData);
+
+
+
 const database = mongoose.connection;
+mongoose.connect(mongoData);
 
 database.on('error', (error) => {
     console.log(error);
@@ -21,11 +24,50 @@ database.once('connected', () => {
 
 
 const app = express();
+const routes = require('./routes/routes');
+
+// const swaggerAutogen = require('swagger-autogen')();
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger.json');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            version: "1.0.0",
+            title: "Vineet API",
+            description: "API Information",
+            contact: {
+                name: "Stupid Developer"
+            },
+            servers: ["http://localhost:3000"]
+        }
+    },
+    // ['.routes/*.js']
+    apis: ["index.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Routes
+/**
+ * @swagger
+ * /api/getAll:
+ *  get:
+ *    description: Use to request all patients
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+
+
+
+
 app.use(express.json());
 app.use(compression());
-
-
-const routes = require('./routes/routes');
 
 app.use('/api', routes);
 
